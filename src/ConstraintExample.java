3866d2bd4d87c540c79c93bf7f58c9a5ba02afda
Antonio Rad <antoniorad@Antonios-MacBook-Pro.local>
2025-11-01 16:49:17 +0200

    add example

---
+++ src/ConstraintExample.java
@@ -0,0 +1,43 @@
+import org.jgap.*;
+import org.jgap.impl.*;
+
+public class ConstraintExample {
+    private static final int NUM_EVOLUTIONS = 100;
+    public static void main(String[] args) throws InvalidConfigurationException {
+        double targetAmount = 1.84;
+        Configuration conf = new DefaultConfiguration();
+        Configuration.resetProperty(Configuration.PROPERTY_FITEVAL_INST);
+        conf.setFitnessEvaluator(new DefaultFitnessEvaluator());
+        conf.setPreservFittestIndividual(true);
+        conf.setKeepPopulationSizeConstant(true);
+
+        FitnessFunction fitnessFunction = new SampleFitnessFunction(targetAmount);
+        conf.setFitnessFunction(fitnessFunction);
+
+        Gene[] sampleGenes = new Gene[4];
+        sampleGenes[0] = new IntegerGene(conf, 0, 20); // Quarters
+        sampleGenes[1] = new IntegerGene(conf, 0, 30); // Dimes
+        sampleGenes[2] = new IntegerGene(conf, 0, 50); // Nickels
+        sampleGenes[3] = new IntegerGene(conf, 0, 80); // Pennies
+        IChromosome sampleChromosome = new Chromosome(conf, sampleGenes);
+        conf.setSampleChromosome(sampleChromosome);
+
+        conf.setPopulationSize(80);
+        Genotype population = Genotype.randomInitialGenotype(conf);
+
+        for (int i = 0; i < NUM_EVOLUTIONS; i++) {
+            population.evolve();
+            IChromosome bestSolutionSoFar = population.getFittestChromosome();
+            DisplayIndividual(bestSolutionSoFar);
+        }
+    }
+    public static void DisplayIndividual(IChromosome chromosome) {
+        System.out.print("Fitness Value: " + chromosome.getFitnessValue());
+        System.out.print(", Coins: ");
+        for (int i = 0; i < 4; i++)
+            System.out.print(SampleFitnessFunction.getNrCoinsAtGene(chromosome, i) + " ");
+        System.out.println(", total change: " + SampleFitnessFunction.amountOfChange(chromosome));
+
+    }
+}
+
---
+++ src/SampleFitnessFunction.java
@@ -0,0 +1,38 @@
+import org.jgap.FitnessFunction;
+import org.jgap.IChromosome;
+
+public class SampleFitnessFunction extends FitnessFunction {
+    private final double targetAmount;
+
+    public SampleFitnessFunction(double targetAmount) {
+        this.targetAmount = targetAmount;
+    }
+    public double evaluate(IChromosome chromosome) {
+        double changeAmount = amountOfChange(chromosome);
+        int totalCoins = getTotalNumberOfCoins(chromosome);
+
+        double changeDifference = Math.abs(targetAmount - changeAmount);
+        double fitness = 300 * changeDifference * changeDifference;
+        fitness += totalCoins > 1 ? totalCoins : 0;
+        return fitness;
+    }
+
+    public static double amountOfChange(IChromosome chromosome) {
+        int numQuarters = getNrCoinsAtGene(chromosome, 0);
+        int numDimes = getNrCoinsAtGene(chromosome, 1);
+        int numNickels = getNrCoinsAtGene(chromosome, 2);
+        int numPennies = getNrCoinsAtGene(chromosome, 3);
+        return (numQuarters * 0.25) + (numDimes * 0.1) + (numNickels * 0.05) + (numPennies * 0.01);
+    }
+
+    public static int getNrCoinsAtGene(IChromosome chromosome, int position) {
+        Integer numCoins = (Integer)chromosome.getGene(position).getAllele();
+        return numCoins.intValue();
+    }
+    public static int getTotalNumberOfCoins(IChromosome chromosome) {
+        int totalCoins = 0;
+        for (int i = 0; i < chromosome.size(); i++)
+            totalCoins += getNrCoinsAtGene(chromosome, i);
+        return totalCoins;
+    }
+}
